30465ab50ef0ad0ebc90eec182fbbc167b6d62fc
MadalinaOtvos <otvos_madalina@yahoo.com>
2015-11-11 00:07:01 +0100

    Fix project login

---
+++ Canvas/app/src/main/java/edu/ubbcluj/canvasAndroid/CanvasApplication.java
@@ -0,0 +1,10 @@
+package edu.ubbcluj.canvasAndroid;
+
+import android.app.Application;
+
+/**
+ * Created by Covaci on 10/23/2015.
+ */
+public class CanvasApplication extends Application {
+    private static  String message = "asdfgh";
+}
