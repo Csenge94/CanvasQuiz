02077248cc1ce94ada03bb597d226ed37db69092
Kulcsar David <kulcsar_david@yahoo.com>
2015-12-06 15:24:08 +0200

    Show Quiz Questions in Quiz Activity

--- CanvasAndroid/app/src/androidTest/java/canvas/ubbcluj/edu/ApplicationTest.java
+++ CanvasAndroid/app/src/androidTest/java/canvas/ubbcluj/edu/ApplicationTest.java
@@ -1,4 +1,4 @@
-package canvas.ubbcluj.edu.canvasandroid;
+package canvas.ubbcluj.edu;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- CanvasAndroid/app/src/main/java/edu/ubbcluj/canvasAndroid/util/listener/InformationListener.java
+++
@@ -1,7 +0,0 @@
-package edu.ubbcluj.canvasAndroid.util.listener;
-
-import java.util.EventListener;
-
-public interface InformationListener extends EventListener {
-    public void onComplete(InformationEvent e);
-}
